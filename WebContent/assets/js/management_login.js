fb1fa214d30af1f3f843df618a0931fdf83c596f
2jz <2jz@R1241PC075.jz.jec.ac.jp>
2024-01-31 14:44:51 +0900

    1月31日（水）ログイン処理完了（参照先は管理者ユーザーテーブルに変える予定）

---
+++ WebContent/assets/js/management_login.js
@@ -0,0 +1,47 @@
+(function(){
+  'use strict';
+  
+  const email = document.querySelector("#email"); // IDセレクタを使用
+  const password = document.querySelector("#password"); // IDセレクタを使用
+  const login = document.querySelector('#login_btn');
+
+  login.addEventListener('click', (e) => {
+    sendData();
+  });
+
+  function transition() {
+    console.log('sessionStorage : ' + sessionStorage.getItem('user_id'));
+    location.href = `/22jz-promo-22jz-g19-management/order_list`;
+  }
+
+  function sendData() {
+    const xhr = new XMLHttpRequest();
+    const data = {
+      email: email.value,
+      password: password.value
+    };
+    // console.log(data);
+    // xhr.open('POST', '/claft-lether/Login', true);
+    xhr.open('POST', '/22jz-promo-22jz-g19-management/Login', true);
+    xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');
+    xhr.send(JSON.stringify(data));
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+          console.log(response);
+          if (response >= 0) {
+            console.log('user_id : ' + response);
+            sessionStorage.setItem('user_id', response);
+            transition();
+          } else {
+            console.log('登録できないお');
+          }
+        } else {
+          console.error('通信エラーが発生しました。');
+        }
+      }
+    };
+  }
+})();
